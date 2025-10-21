e0b2c18d2aa65969de3462d07e7b1512a1adf6be
Marian Pecqueur <marpecqueur@gmail.com>
2025-10-21 14:53:00 +0200

    im a debil

--- src/cmd_handler.rs
+++ src/cmd_handler.rs
@@ -34,7 +34,7 @@ pub fn handle_cmd(command: &mut String) {
             file_tree::fs_system.lock().serialize(head, None);
         }
         "mkdir" => {
-            fs_system.lock().mkdir(rest.to_string());
+            fs_system.lock().mkdir(rest.as_str());
         }
         "cd" => {
             unsafe { fs_system.force_unlock() };
